1b66b48ad160f394c378df5a245a96b92f18d937
Parv-gugnani <9765.parv@gmail.com>
2023-10-17 09:47:05 +0530

    comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import Header from "./components/Header";
+import Userinput from "./components/Userinput";
+import ResultsTable from "./components/ResultsTable";
 
 function App() {
   const [userInput, setUserInput] = useState(null);
@@ -10,7 +12,25 @@ function App() {
 
   const yearlyData = [];
 
-  if (userInput) return <div className="App"></div>;
-}
+  if (userInput){
+    let currentSaving = +userInput['current-saving'];
+    const yearlyContri = +userInput['yearly-contribution'];
+    const expectedRetr = +userInput['expected-return']/100;
+    const duration = +userInput['duration'];
 
-export default App;
+    for (let i = 0; i <duration; i++) {
+      const yearlyIntst =  currentSaving + expectedRetr ;
+      currentSaving += yearlyIntst; + yearlyContri;
+      yearlyContri.push({
+        year:i+1,
+        yearlyIntst:yearlyIntst,
+        savingEndofYear:currentSaving,
+        yearlyContri:yearlyContri,
+      })
+  }
+
+    //
+    return <div className="App">
+      <header/>
+      
+    </div>;
---
+++ src/components/Userinput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Userinput() {
+  return <div></div>;
+}
+
+export default Userinput;
